b63122303c71ed4347677d2e17ff50c5d4713193
Luffka <33230841+luffkaa@users.noreply.github.com>
2021-11-09 11:39:04 +0100

    Added positioning for Header and Send Message field

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,6 +1,6 @@
 import styles from "./index.module.css";
 import { EnterNameContainer } from "../EnterName";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { HeaderProps } from "./types";
 
 export default function Header({
@@ -8,14 +8,25 @@ export default function Header({
   handleCurrentAuthor,
 }: HeaderProps) {
   const [editName, setEditName] = useState<boolean>(false);
+  const [isScrolling, setIsScrolling] =  useState<boolean>(false);
 
   const handleEditName = () => {
     setEditName(!editName)
   }
 
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+        setIsScrolling(window.scrollY > 10);
+    });
+
+    return () => window.removeEventListener("scroll", () => {
+      setIsScrolling(false);
+    })
+  }, []);
+
   return (
     <header
-      className={styles.header}
+      className={`${styles.header} ${isScrolling ? styles.header_scrolling : ''}`}
     >
       <div className={styles.header__name_container}>
         <h1
